334d3cec5745e7b6185967dab4ea775f7c99cfc0
Uğurcan Eser <ugurcaneser@yahoo.com>
2025-01-03 15:15:29 +0300

    add home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import "./global.css"
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-2xl text-black'>Welcome to Budgetory!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeScreen />
   );
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-2xl text-black">Open up App.tsx to start working on your app!</Text>
+        </View>
+    )};
