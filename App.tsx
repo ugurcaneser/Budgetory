e705097cec3f9b4e62f07ed1e5f1564a6ac4b888
Uğurcan Eser <ugurcaneser@yahoo.com>
2025-01-05 23:52:14 +0300

    made bug fixes

--- App.tsx
+++ App.tsx
@@ -4,31 +4,16 @@ import "./global.css"
 import HomeScreen from './screens/HomeScreen';
 import { CurrencyProvider } from './context/CurrencyContext';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-export type RootStackParamList = {
-  Home: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
+        <StatusBar style="auto" />
         <NavigationContainer>
           <CurrencyProvider>
-            <Stack.Navigator 
-              initialRouteName="Home" 
-              screenOptions={{ 
-                headerShown: false,
-                animation: 'slide_from_right'
-              }}
-            >
-              <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-            <StatusBar style="auto" />
+            <HomeScreen />
           </CurrencyProvider>
         </NavigationContainer>
       </SafeAreaProvider>
