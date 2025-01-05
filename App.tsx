ef36669661a018607792445f49fe529cf3a5d9bd
Uğurcan Eser <ugurcaneser@yahoo.com>
2025-01-05 23:23:45 +0300

    prepare to build

--- App.tsx
+++ App.tsx
@@ -5,20 +5,33 @@ import HomeScreen from './screens/HomeScreen';
 import { CurrencyProvider } from './context/CurrencyContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <CurrencyProvider>
-          <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </Stack.Navigator>
-          <StatusBar style="auto" />
-        </CurrencyProvider>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <CurrencyProvider>
+            <Stack.Navigator 
+              initialRouteName="Home" 
+              screenOptions={{ 
+                headerShown: false,
+                animation: 'slide_from_right'
+              }}
+            >
+              <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+          </CurrencyProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
