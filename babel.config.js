eeea1306cd6dbb344011931b5856ff914a037464
Uğurcan Eser <ugurcaneser@yahoo.com>
2025-01-12 14:15:56 +0300

    fix some build errors

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,12 @@ module.exports = function (api) {
         ["babel-preset-expo", { jsxImportSource: "nativewind" }],
         "nativewind/babel",
       ],
+      plugins: [
+        ["@babel/plugin-transform-class-properties", { loose: true }],
+        ["@babel/plugin-transform-private-methods", { loose: true }],
+        ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+        "@babel/plugin-transform-nullish-coalescing-operator",
+        "@babel/plugin-transform-optional-chaining"
+      ]
     };
   };
